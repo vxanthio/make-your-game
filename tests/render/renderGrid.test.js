5fc779a9eb92595beb75286ac5d27c2d52dac3b6
vxanthio <basiliki.martios1@gmail.com>
2026-09-09 21:09:52 +0300

    test: add playfield cell count test

---
+++ tests/render/renderGrid.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest";
+import { createPlayfieldDOM } from "../../src/render/renderGrid.js";
+test("create a playfield DOM with the correct number of cells", ()=> {
+const playfield= createPlayfieldDOM({
+    rows:11,
+    cols:13
+});
+const cells=playfield.querySelectorAll(".playfield__cell");
+expect(cells.length).toBe(143);
+});
