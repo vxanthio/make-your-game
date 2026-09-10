e937242fbd19a4b60a676f06e767d369f917ac5c
vxanthio <basiliki.martios1@gmail.com>
2026-09-10 15:40:28 +0300

    feat: allow movement on exit cells

--- src/logic/movement.js
+++ src/logic/movement.js
@@ -1,5 +1,8 @@
 export function isWalkable(grid, row, col) {
-    if (grid[row][col]==='empty') {
-        return true;
-    }
+  if (grid[row][col] === 'empty') {
+    return true;
+  }
+  if (grid[row][col] === 'exit') {
+    return true;
+  }
 }
