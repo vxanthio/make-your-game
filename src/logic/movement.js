9ad4701e1ebee10eef4e8f68b7bfb0d7eae6060c
vxanthio <basiliki.martios1@gmail.com>
2026-09-10 16:01:24 +0300

    feat: block movement through soft cells

--- src/logic/movement.js
+++ src/logic/movement.js
@@ -5,7 +5,10 @@ export function isWalkable(grid, row, col) {
   if (grid[row][col] === 'exit') {
     return true;
   }
-  if (grid[row][col]==='wall') {
+  if (grid[row][col] === 'wall') {
+    return false;
+  }
+  if (grid[row][col] === 'soft') {
     return false;
   }
 }
