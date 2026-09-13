970d97697cfc95ca8859ff086967180720358640
vxanthio <basiliki.martios1@gmail.com>
2026-09-13 15:06:31 +0300

    test: add downward movement case

--- tests/logic/movement.test.js
+++ tests/logic/movement.test.js
@@ -48,16 +48,29 @@ test('increase x possition when entity goes right', () => {
   expect(entity.x).toBe(3);
   expect(entity.y).toBe(0);
 });
-test('decrease x possition when entity goes left',() =>{
-  const entity ={
-    x:10,
-    y:0,
-    speed:0.15,
+test('decrease x possition when entity goes left', () => {
+  const entity = {
+    x: 10,
+    y: 0,
+    speed: 0.15,
   };
-  const dt=20;
-  const direction='left';
-  const grid=[['empty','empty']];
-  moveEntity(entity,dt,direction,grid);
+  const dt = 20;
+  const direction = 'left';
+  const grid = [['empty', 'empty']];
+  moveEntity(entity, dt, direction, grid);
   expect(entity.x).toBe(7);
   expect(entity.y).toBe(0);
+});
+test('increase y possition when the entity goes down', () => {
+  const entity = {
+    x: 0,
+    y: 10,
+    speed: 0.15,
+  };
+  const dt = 20;
+  const direction = 'down';
+  const grid = [['empty', 'empty']];
+  moveEntity(entity, dt, direction, grid);
+  expect(entity.y).toBe(13);
+  expect(entity.x).toBe(0);
 });
