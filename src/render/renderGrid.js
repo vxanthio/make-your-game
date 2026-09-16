c5ad709e81559f16fafb7eb2184910e897f3396c
vxanthio <basiliki.martios1@gmail.com>
2026-09-16 13:58:59 +0300

    feat: render soft cell modifier class

--- src/render/renderGrid.js
+++ src/render/renderGrid.js
@@ -14,11 +14,13 @@ export function createPlayfieldDOM(config) {
 export function renderGrid(grid) {
   const playfield = document.querySelector('.playfield');
   const cells = playfield.querySelectorAll('.playfield__cell');
-  for (let row=0;row<grid.length;row++) {
-    for (let col=0;col<grid[row].length;col++) {
-  if (grid[row][col].type === 'wall') {
-    cells[row*grid[row].length+col].classList.add('playfield__cell--wall');
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (grid[row][col].type === 'wall') {
+        cells[row * grid[row].length + col].classList.add('playfield__cell--wall');
+      } else if (grid[row][col].type === 'soft') {
+        cells[row * grid[row].length + col].classList.add('playfield__cell--soft');
+      }
+    }
   }
-}
-}
 }
