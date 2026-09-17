bff4f7252f3ce91a1f2a9d7362509baf21920a13
vxanthio <basiliki.martios1@gmail.com>
2026-09-17 21:28:58 +0300

    feat: position enemies using transform

--- src/render/renderEntities.js
+++ src/render/renderEntities.js
@@ -1,4 +1,8 @@
 export function renderEntities(state) {
   const player = document.querySelector('.sprite--player');
   player.style.transform = `translate(${state.player.x}px, ${state.player.y}px)`;
+  for (const enemy of state.enemies) {
+    const enemyElement = document.querySelector(`.sprite--enemy[data-entity-id='${enemy.id}']`);
+    enemyElement.style.transform = `translate(${enemy.x}px, ${enemy.y}px)`;
+  }
 }
