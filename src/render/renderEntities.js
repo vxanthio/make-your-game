f285836c3a3417a69f1d203aec9c6a16e992ccbc
vxanthio <basiliki.martios1@gmail.com>
2026-09-18 20:06:31 +0300

    feat: position explosion cells using grid coordinates

--- src/render/renderEntities.js
+++ src/render/renderEntities.js
@@ -10,4 +10,10 @@ export function renderEntities(state) {
     const bombElement = document.querySelector(`.sprite--bomb[data-entity-id='${bomb.id}']`);
     bombElement.style.transform = `translate(${bomb.col * TILE_SIZE_PX}px, ${bomb.row * TILE_SIZE_PX}px)`;
   }
+  for (const explosion of state.explosions) {
+    for (const [index, cell] of explosion.cells.entries()) {
+  const explosionElement= document.querySelector(`.sprite--explosion[data-entity-id='${explosion.id}'][data-cell-index='${index}']`);
+  explosionElement.style.transform=`translate(${cell.col*TILE_SIZE_PX}px, ${cell.row*TILE_SIZE_PX}px)`;
+  }
+}
 }
